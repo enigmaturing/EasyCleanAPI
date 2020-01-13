d15b65c5ae98a158bce85594af386323aa74eced
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-01-13 18:33:04 +0100

    Add class FileHelper.cs
    (Nr 22) This class contains the method UploadPhoto() and writes all bytes of a stream into a file in a given path

---
+++ EasyClean/Helpers/FileHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace EasyClean.Helpers
+{
+    public class FileHelper
+    {
+        public static bool UploadPhoto(MemoryStream memoryStream, string folderName, string fileName)
+        {
+            try
+            {
+                memoryStream.Position = 0;
+                var path = Path.Combine(HttpContext.Current.Server.MapPath(folderName), fileName).Replace("\\", "/");
+                File.WriteAllBytes(path, memoryStream.ToArray());
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
