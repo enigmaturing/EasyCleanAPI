d8516a37be60ebb32aab81fb0960f9acdc893323
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-01-13 18:03:40 +0100

    Create model class EndClient.cs
    (Nr 22) This class serves as a model so that Entity Framework creates a table for our DB

---
+++ EasyClean/Models/EndClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EasyClean.Models
+{
+    public class EndClient
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(20)]
+        public string UserName { get; set; }
+
+        [Required, StringLength(20)]
+        public string UserSurname { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public int DateOfBirth { get; set; }
+        public string ImagePath { get; set; }
+
+        [NotMapped]
+        public byte[] ImageArray { get; set; }
+    }
+}
